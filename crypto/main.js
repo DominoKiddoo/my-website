c3e99aa6fb307f9771866e0e44a0eb8632f25c1a
DominoKiddoo <lucas.ellul@gmail.com>
2026-05-30 17:36:37 -0400

    introducing my new very real cryptocurrency

---
+++ crypto/main.js
@@ -0,0 +1,10 @@
+const button = document.getElementById("scam");
+const video = document.getElementById("lmao");
+function doClick() {
+    button.remove();
+    title.remove();
+    video.style.display = "block";
+    document.body.style.backgroundImage = "none";
+    video.play();
+}
+
